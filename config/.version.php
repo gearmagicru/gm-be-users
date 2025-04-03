f27fdcb8393a2f862f890cf88cb1b731ce1bfc0c
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-04-03 22:12:59 +0300

    Обновлена версия

--- config/.version.php
+++ config/.version.php
@@ -12,8 +12,8 @@
 return [
     'name'        => 'Users',
     'description' => 'Control Panel and Website User Accounts',
-    'version'     => '1.0',
-    'versionDate' => '20-12-2017',
+    'version'     => '1.0.1',
+    'versionDate' => '03-04-2025',
     'author'      => 'GearMagic',
     'authorUrl'   => 'https://gearmagic.ru',
     'email'       => 'anton.tivonenko@gmail.com',
